902c12a87693b490958ecda283f6bd3a795b6029
Digambar <digambardeshmukh12@gmail.com>
2024-01-26 16:58:15 +0530

    By Digambar

--- portfolio/src/app/components/selecters/projects/projects.component.ts
+++ portfolio/src/app/components/selecters/projects/projects.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+// projects.component.ts
+
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+ 
+  ngOnInit(): void {}
+   imageLoaded = false;
 
+  onImageLoad() {
+    this.imageLoaded = true;
+  }
+
+  @ViewChild('image', { static: false }) imageElement: ElementRef | undefined;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (this.imageElement) {
+      this.startSlowLoad();
+      this.addHoverEffect();
+    }
+  }
+
+  startSlowLoad() {
+    const image = this.imageElement!.nativeElement; // Use definite assertion
+
+    let opacity = 0;
+    const interval = setInterval(() => {
+      opacity += 0.2;
+      this.renderer.setStyle(image, 'opacity', opacity);
+
+      if (opacity >= 1) {
+        clearInterval(interval);
+      }
+    }, 300);
+  }
+
+
+  addHoverEffect() {
+    const image = this.imageElement!.nativeElement;
+
+    this.renderer.listen(image, 'mouseover', () => {
+      this.renderer.addClass(image, 'hover-effect');
+    });
+
+    this.renderer.listen(image, 'mouseout', () => {
+      this.renderer.removeClass(image, 'hover-effect');
+    });
+  }
 }
