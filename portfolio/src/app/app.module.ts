0a46623a0cb42d96801669da01e6c64ee52fdfba
kbi2000 <helloworldkabir@gmail.com>
2024-01-26 13:42:36 +0530

    by kabir

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsdetailsComponent } from './components/projectsdetails/projectsdetails.component';
+import { ProjectsComponent } from './components/selecters/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsdetailsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/components/projectsdetails/projectsdetails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projectsdetails',
+  templateUrl: './projectsdetails.component.html',
+  styleUrls: ['./projectsdetails.component.css']
+})
+export class ProjectsdetailsComponent {
+
+}
