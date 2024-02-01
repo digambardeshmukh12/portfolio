99c5ec83a9588d1c22ec8786e1ebbab9b5136b10
Digambar <digambardeshmukh12@gmail.com>
2024-02-02 00:58:11 +0530

    By Kabir Changes

--- portfolio/src/app/components/selecters/project-cards/project-cards.component.ts
+++ portfolio/src/app/components/selecters/project-cards/project-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-project-cards',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-cards.component.css']
 })
 export class ProjectCardsComponent {
+  @ViewChild('marquee', { static: true }) marquee!: ElementRef;
 
+  cards = [1, 2, 3, 4, 5];
+
+  stopMarquee() {
+    if (this.marquee) {
+      this.marquee.nativeElement.stop();
+    }
+  }
+
+  startMarquee() {
+    if (this.marquee) {
+      this.marquee.nativeElement.start();
+    }
+  }
 }
